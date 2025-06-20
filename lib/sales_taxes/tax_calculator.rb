d3fb829c236739048f0fea20213840c542c5aae9
Paulo Fabiano Langer <paulofabiano@gmail.com>
2025-06-20 21:21:23 +0100

    chore: implement tax calculator using tax rules

--- lib/sales_taxes/tax_calculator.rb
+++ lib/sales_taxes/tax_calculator.rb
@@ -7,6 +7,17 @@ module SalesTaxes
   class TaxCalculator
     ROUND_FACTOR = 0.05.to_d.freeze
 
+    def initialize(rules = [BasicTaxRule.new, ImportTaxRule.new])
+      @rules = rules.freeze
+    end
+
+    def tax_for(product)
+      sum = @rules
+            .select { |rule| rule.applicable?(product) }
+            .sum    { |rule| product.net_price * rule.rate }
+      round_up(sum)
+    end
+
     private
 
     # Ceil to next 0.05, keep 2 decimals
