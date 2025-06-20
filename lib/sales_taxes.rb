873a28caa22b6dd8d759ae3b1d062511f0fa43ec
Paulo Fabiano Langer <paulofabiano@gmail.com>
2025-06-20 21:19:43 +0100

    chore: require new files

--- lib/sales_taxes.rb
+++ lib/sales_taxes.rb
@@ -5,6 +5,9 @@ require 'bigdecimal/util'
 
 require_relative 'sales_taxes/tax_calculator'
 require_relative 'sales_taxes/product'
+require_relative 'sales_taxes/basic_tax_rule'
+require_relative 'sales_taxes/import_tax_rule'
+require_relative 'sales_taxes/tax_rule'
 
 module SalesTaxes
   VERSION = '0.1.0'
