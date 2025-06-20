0fa1234a9a0ba01af6c63ae67efa2b970e62e2a8
Paulo Fabiano Langer <paulofabiano@gmail.com>
2025-06-20 22:35:51 +0100

    chore: require basket and basket item

--- lib/sales_taxes.rb
+++ lib/sales_taxes.rb
@@ -8,6 +8,10 @@ require_relative 'sales_taxes/product'
 require_relative 'sales_taxes/basic_tax_rule'
 require_relative 'sales_taxes/import_tax_rule'
 require_relative 'sales_taxes/tax_rule'
+require_relative 'sales_taxes/basket_item'
+require_relative 'sales_taxes/basket'
+require_relative 'sales_taxes/input_parser'
+require_relative 'sales_taxes/receipt'
 
 module SalesTaxes
   VERSION = '0.1.0'
